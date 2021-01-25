08afb505d43a0b91fb77c0e506fb09d191c26bf5
Jose Tow Echenique <jose.towe@gmail.com>
2021-01-25 11:08:25 -0600

    Added hook for Lists of items

--- Client/Web/src/utils/hooks.js
+++ Client/Web/src/utils/hooks.js
@@ -50,3 +50,37 @@ export const useForm = (callback, initialState = {}) => {
   };
 
 }
+
+/** useList
+ * Contains the hooks and callbacks for the state of an array of items, intended
+ * to be used as a list
+ * 
+ * @param initialState The array
+ * 
+ * @returns values     (array with the read-only values for binding)
+ * @returns append     (adds an element to the array)
+ * @returns loading    (State that indicates if the loading spinner should be shown)
+ * @returns setLoading (Function that allows the loading state to be changed)
+ */
+export const useList = (initialState = []) => {
+  const [values, setValues] = useState(initialState);
+  const [loading, setLoading] = useState(false);
+
+  const append = (object) => {
+    const nuArr = [...values, object];
+    setValues(nuArr);
+  };
+
+  const appendMulti = (array) => {
+    const nuArr = [...values, ...array];
+    setValues(nuArr);
+  }
+
+  return {
+    append,
+    appendMulti,
+    loading,
+    setLoading,
+    values,
+  };
+}
